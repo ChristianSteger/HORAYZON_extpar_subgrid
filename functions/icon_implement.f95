3d51215f135f18084c31b20c1f0dcf4c11776a45
Christian Steger <csteger@balfrin-ln003.cscs.ch>
2025-09-17 10:23:58 +0200

    Fortran ICON implementation completed and tested

--- functions/icon_implement.f95
+++ functions/icon_implement.f95
@@ -18,30 +18,35 @@ SUBROUTINE interpolate(horizon, ztheta_sun, zphi_sun, fcor_sun)
   ! INTEGER, PARAMETER :: wp = sp  ! single precision
   INTEGER, PARAMETER :: wp = dp  ! double precision
 
-  REAL(KIND=wp), DIMENSION(192) :: horizon ! start elevation angle and f_cor-values [degree, -]
-  REAL(KIND=wp) :: zphi_sun   ! sun azimuth angle [rad]
-  REAL(KIND=wp) :: ztheta_sun ! sun elevation angle [rad]
-  REAL(KIND=wp) :: fcor_sun    ! interpolated f_cor_value for sun position [-]
+  REAL(wp), DIMENSION(192) :: horizon ! start elevation angle and f_cor-values [degree, -]
+  REAL(wp) :: zphi_sun   ! sun azimuth angle [rad]
+  REAL(wp) :: ztheta_sun ! sun elevation angle [rad]
+  REAL(wp) :: fcor_sun    ! interpolated f_cor_value for sun position [-] (new variable)
 
-  INTEGER :: nhori
-  REAL(KIND=wp) :: pi, rad2deg
+  ! Variables already defined in ICON
   INTEGER :: k, ii
+  REAL(wp) :: pi, rad2deg
+  INTEGER :: nhori ! currently: 24, new: 24 * 8 = 192 !!!
+  REAL(wp) :: zihor
+
+  ! New variables
+  INTEGER :: num_azim
   INTEGER :: ind_hori
-  REAL(KIND=wp) :: zihor
-  REAL(KIND=wp) :: eta
+  REAL(wp) :: eta
   INTEGER :: num_elem
-  REAL(KIND=wp) :: elev_start, elev_end
-  REAL(KIND=wp) :: pos_norm
-  REAL(KIND=wp) :: fcor_left, fcor_right
+  REAL(wp) :: elev_start, elev_end
+  REAL(wp) :: pos_norm
+  REAL(wp) :: fcor_left, fcor_right
   INTEGER :: ind_low
-  REAL(KIND=wp) :: elev_low, elev_up, weight_low
+  REAL(wp) :: elev_low, elev_up, weight_low
 
   ! Constant values
-  nhori = 24
-  eta = 2.1_wp
+  num_azim = 24
   num_elem = 8
+  nhori = num_azim * num_elem ! 192
+  eta = 2.1_wp
   elev_end = 90.0_wp
-  
+
   !f2py threadsafe
   !f2py intent(in) horizon
   !f2py intent(in) zphi_sun
@@ -53,60 +58,43 @@ SUBROUTINE interpolate(horizon, ztheta_sun, zphi_sun, fcor_sun)
   rad2deg = 180.0_wp / pi
 
   ! Compute azimuth indices
-  zihor = REAL(INT(360.0_wp/nhori),wp) ! 15 degree
-  ii = MIN(nhori-1, INT(rad2deg*zphi_sun/zihor)) ! index starting with 0!
-  k = MOD(ii+1,nhori)                            ! index starting with 0!
-  PRINT *, "ii =", ii, " k =", k
+  zihor = REAL(INT(360.0_wp/num_azim),wp) ! 15 degree
+  ii = MIN(num_azim-1, INT(rad2deg*zphi_sun/zihor)) ! index starting with 0!
+  k = MOD(ii+1,num_azim)                            ! index starting with 0!
 
   ! Left f_cor-value
   ind_hori = ii * num_elem + 1
-  PRINT *, "ind_hori =", ind_hori
   elev_start = horizon(ind_hori)
-  PRINT *, "elev_start =", elev_start
   pos_norm = (ztheta_sun*rad2deg - elev_start) / (elev_end - elev_start)
-  PRINT *, "pos_norm =", pos_norm
   IF (pos_norm <= 0.0) THEN
     fcor_left = 0.0_wp
   ELSE
     ind_low = INT((num_elem - 2) * pos_norm ** (1.0 / eta)) + 1
-    PRINT *, "ind_low =", ind_low
     IF (ind_low >= (num_elem - 1)) THEN
       fcor_left = 1.0_wp
     ELSE
       elev_low = elev_start + (elev_end - elev_start) * (REAL(ind_low-1,wp) / REAL(num_elem - 2,wp)) ** eta
-      PRINT *, "elev_low =", elev_low
       elev_up = elev_start + (elev_end - elev_start) * (REAL(ind_low,wp) / REAL(num_elem - 2,wp)) ** eta
-      PRINT *, "elev_up =", elev_up
       weight_low = (elev_up - ztheta_sun*rad2deg) / (elev_up - elev_low)
-      PRINT *, "weight_low =", weight_low
       fcor_left = horizon(ind_hori+ind_low) * weight_low + horizon(ind_hori+ind_low+1) * (1.0_wp - weight_low)
-      PRINT *, "fcor_left =", fcor_left
     ENDIF
   ENDIF
 
   ! Right f_cor-value
   ind_hori = k * num_elem + 1
-  PRINT *, "ind_hori =", ind_hori
   elev_start = horizon(ind_hori)
-  PRINT *, "elev_start =", elev_start
   pos_norm = (ztheta_sun*rad2deg - elev_start) / (elev_end - elev_start)
-  PRINT *, "pos_norm =", pos_norm
   IF (pos_norm <= 0.0) THEN
     fcor_right = 0.0_wp
   ELSE
     ind_low = INT((num_elem - 2) * pos_norm ** (1.0 / eta)) + 1
-    PRINT *, "ind_low =", ind_low
     IF (ind_low >= (num_elem - 1)) THEN
       fcor_right = 1.0_wp
     ELSE
       elev_low = elev_start + (elev_end - elev_start) * (REAL(ind_low-1,wp) / REAL(num_elem - 2,wp)) ** eta
-      PRINT *, "elev_low =", elev_low
       elev_up = elev_start + (elev_end - elev_start) * (REAL(ind_low,wp) / REAL(num_elem - 2,wp)) ** eta
-      PRINT *, "elev_up =", elev_up
       weight_low = (elev_up - ztheta_sun*rad2deg) / (elev_up - elev_low)
-      PRINT *, "weight_low =", weight_low
       fcor_right = horizon(ind_hori+ind_low) * weight_low + horizon(ind_hori+ind_low+1) * (1.0_wp - weight_low)
-      PRINT *, "fcor_left =", fcor_left
     ENDIF
   ENDIF
 
--- functions/test_icon_implement.py
+++ functions/test_icon_implement.py
@@ -3,6 +3,7 @@
 # Author: Christian R. Steger, September 2025
 
 import numpy as np
+import xarray as xr
 
 from functions.fcor_processing import spacing_exp, spacing_exp_interp
 from functions.icon_implement import interpolate # type: ignore
@@ -11,35 +12,86 @@ from functions.icon_implement import interpolate # type: ignore
 # Test
 ###############################################################################
 
-# Create exponentially spaced array
-x_start = float(np.random.uniform(0.0, 40.0, 1)[0])
-# x_start = 34.6
-print(f"x_start: {x_start:.4f}")
-x_end = 90.0
-num_nodes = 7
+# # Artificial input data
+# num_azim = 24
+# num_elem = 8
+# f_cor_sparse = np.random.uniform(0.0, 5.0, num_azim * num_elem) \
+#     .astype(np.float32) # [-]
+# f_cor_sparse[0:None:num_elem] = 10.0 # starting elevation angle [degrees]
+# # f_cor_sparse[0:None:num_elem] = np.random.uniform(0.0, 35.0, num_azim)
+# f_cor_sparse[1:None:num_elem] = 0.0 # f_cor @ elev_start
+# f_cor_sparse[7:None:num_elem] = 1.0 # f_cor @ elev_end
+
+# Real input data from EXTPAR
+num_azim = 24
+num_elem = 8
+file = "/scratch/mch/csteger/ICON-CH1-EPS_copy/" \
+    + "external_parameter_icon_grid_0001_R19B08_mch_tuned_f_cor_sparse.nc"
+ds = xr.open_dataset(file)
+num_gc_icon = ds["cell"].size
+# ind_loc = 750_000
+# ind_loc = 790_610
+# ind_loc = 777125 # total shadow until ca. ~25 deg
+# ind_loc = 680_000 # always below 1.0
+ind_loc = 580_000  # up to 6.0
+ind_loc = np.random.randint(0, num_gc_icon, 1)[0]
+f_cor_sparse = ds["HORIZON"][:, ind_loc].values
+ds.close()
+
+# Settings
+zphi_sun = np.deg2rad(np.random.uniform(0.0, 360.0, 1)[0])
+# zphi_sun = np.deg2rad(360.0)
+# sun azimuth angle [rad]
+ztheta_sun = np.deg2rad(np.random.uniform(0.0, 90.0, 1)[0])
+# ztheta_sun = np.deg2rad(f_cor_sparse[0]) + 0.0000000001
+# ztheta_sun = np.deg2rad(+90.3001)
+# sun elevation angle [rad]
+print(f"Sun position: azimuth {np.rad2deg(zphi_sun):.2f} deg," +
+      f" elevation {np.rad2deg(ztheta_sun):.2f} deg")
+
+# -----------------------------------------------------------------------------
+# Python (Numba) implementation
+# -----------------------------------------------------------------------------
+print("Python (Numba) implementation".center(60, '-'))
+
+# Azimuth indices
+ind_azim_left = np.minimum(int(zphi_sun / np.deg2rad(360.0 / num_azim)),
+                           num_azim - 1)
+ind_azim_right = (ind_azim_left + 1) % num_azim
+
+# Fixed settings
 eta = 2.1
-x_spac = spacing_exp(x_start, x_end, num_nodes, eta)
+elev_end = 90.0
 
-# Check interpolation
-x_ip = np.random.uniform(x_start, x_end, 1)[0]
-# x_ip = 85.7
-# x_ip = x_start - 0.00001
-x_ip = x_end - 0.000000000000001
-y = np.random.uniform(0.0, 1.0, num_nodes).astype(np.float32)
-y[0], y[-1] = 0.0, 1.0
-y_ip = spacing_exp_interp(x_start, x_end, num_nodes, eta, x_ip, y)
-if abs(y_ip - np.interp(x_ip, x_spac, y, left=0.0, right=1.0)) > 1e-6:
-    raise ValueError("Interpolation erroneous")
-print(f"y_ip: {y_ip:.4f}")
+# Left and right f_cor-values
+f_cor_values = np.empty(2, dtype=np.float32)
+for ind_i, ind_azim in enumerate([ind_azim_left, ind_azim_right]):
+    ind_azim_start = ind_azim * num_elem
+    elev_start = f_cor_sparse[ind_azim_start]
+    f_cor_sparse_azim \
+        = f_cor_sparse[ind_azim_start + 1:ind_azim_start + num_elem]
+    f_cor_values[ind_i] = spacing_exp_interp(
+        elev_start, elev_end, num_elem - 1, eta, np.rad2deg(ztheta_sun),
+        f_cor_sparse_azim)
+    x_spac = spacing_exp(elev_start, elev_end, num_elem - 1, eta)
+    f_cor_check = np.interp(np.rad2deg(ztheta_sun), x_spac, f_cor_sparse_azim,
+                            left=0.0, right=1.0)
+    if abs(f_cor_check - f_cor_values[ind_i]) > 1e-6:
+        raise ValueError("Interpolation erroneous")
+with np.printoptions(precision=4, suppress=True):
+    print(f"f_cor values (left, right): {f_cor_values}")
 
-horizon = np.empty(24 * 8, dtype=np.float32)
-horizon[0] = x_start
-horizon[1:8] = y
-horizon[8] = x_start
-horizon[9:16] = y
+# Compute f_cor for sun position
+azim_spac = 360.0 / num_azim
+weight_right = (np.rad2deg(zphi_sun) - ind_azim_left * azim_spac) / azim_spac
+f_cor_sun = f_cor_values[0] * (1.0 - weight_right) \
+    + f_cor_values[1] * weight_right
+print(f"fcor_sun: {f_cor_sun:.4f}")
 
-zphi_sun = np.deg2rad(7.5) # sun azimuth angle [rad]
-ztheta_sun = np.deg2rad(x_ip) # sun elevation angle [rad]
+# -----------------------------------------------------------------------------
+# ICON (Fortran) implementation
+# -----------------------------------------------------------------------------
+print("ICON (Fortran) implementation".center(60, '-'))
 
-fcor_sun = interpolate(horizon, ztheta_sun, zphi_sun)
-print(f"fcor_sun: {fcor_sun:.4f}")
+f_cor_sun = interpolate(f_cor_sparse, ztheta_sun, zphi_sun)
+print(f"fcor_sun: {f_cor_sun:.4f}")
